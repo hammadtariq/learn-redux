d0eab0de6a400ec60c1c6522fe353864c8fe7a7c
Hammad Tariq <hammadtariq65@gmail.com>
2016-08-31 19:38:33 +0500

    fetching api added

--- step04_asyncMiddleware/index.js
+++ step04_asyncMiddleware/index.js
@@ -1,14 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger'; //for nice looking logs 
-import thunk from 'redux-thunk'; //for multiple synchronous action
+import thunk from 'redux-thunk'; //for multiple asynchronous action
+import axios from 'axios';
 
-const reducer = ( state, action )=>{
+const initialState = {
+    fetched:false,
+    fetching:false,
+    err:null,
+    user:[],
+}
+
+const reducer = ( state=initialState, action )=>{
     switch(action.type){
-        case "INC":
-            state = state+1
+        case "FETCH_USERS_START":
+            state = Object.assign({}, state, {fetching:true})
         break;
-        case "DEC":
-            state = state-1
+        case "USER_RECIEVED":
+            state = Object.assign({}, state, {fetched:true, fetching:false, user: action.payload})
+        break;
+        case "FETCH_USERS_ERROR":
+            state = Object.assign({}, state, {fetching:false, err:action.payload})
         break;
     }
     return state;
@@ -18,8 +29,14 @@ const middlewear = applyMiddleware(thunk, logger());
 
 const store = createStore(reducer, 0, middlewear);
 
-store.dispatch((dispatch)=>{
-    dispatch({type:"INC"})
-    dispatch({type:"INC"})
-    dispatch({type:"DEC"})
+store.dispatch((dispatch) => {
+    //we are able to do multiple actions bcoz of thunk modules
+    dispatch({type:"FETCH_USERS_START"})
+    axios.get('http://rest.learncode.academy/api/wstern/users')
+        .then((response)=>{
+            dispatch({type:"USER_RECIEVED", payload:response.data})
+        })
+        .catch((err)=>{
+            dispatch({type:"FETCH_USERS_ERROR", payload:response.data})
+        })
 });
